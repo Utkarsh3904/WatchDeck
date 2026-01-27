c9bde1ee096b9375705db7bbced33aa187901057
Utkarsh3904 <millenniumatom@gmail.com>
2026-01-27 21:56:35 +0530

    middlewares added in app.js

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser" //user ki cookies ko access n set krpau iss se
+
+
+const app=express()
+
+// using use is for middleware
+app.use(cors({
+    origin: process.env.CORS_ORIGIN, //for production , extra 
+    credentials:true
+}))
+app.use(express.json({limit : "16kb"}))
+app.use(express.urlencoded({extended: true, limit :"16kb"}))
+app.use(express.static("public"))//public assets/folder that stores photos etc in local server
+app.use(cookieParser())
+
+export {app}
