0ca58962e5355547901f959951ece7adea81a1af
Utkarsh3904 <millenniumatom@gmail.com>
2026-02-01 01:41:54 +0530

    video model

---
+++ src/models/video.model.js
@@ -0,0 +1,50 @@
+import moongoose , {Schema} from 'mongoose'
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required : true,
+            unique : true,
+            lowercase :true,
+            trim : true,
+            index: true    //for to enable search on basis on this (optimizes search)
+        },
+        email: {
+            type: String,
+            required : true,
+            unique : true,
+            lowercase :true,
+            trim : true,
+        },
+        username: {
+            type: String,
+            required : true,
+            trim : true,
+            index: true    //for to enable search on basis on this (optimizes search)
+        },
+        avatar:{
+            type: String,     //cloudinary URL
+            required: true,
+        },
+        coverImage :{
+            type: String,
+        },
+        watchHistory : [   //array
+            {
+                type: Schema.Types.ObjectId,
+                ref : "Video"
+            }
+        ],
+        password :{
+            type : String,
+            required: [true, 'Passwrd is required']   //just a message 
+        },
+        refreshToken: {
+            type : String
+        }
+    },
+    { timestamps : true }
+)
+
+export const User = moongoose.model( "User" , userSchema)
