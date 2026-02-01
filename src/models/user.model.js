3448244007c9dc16f852cd238fcfada10ce62925
Utkarsh3904 <millenniumatom@gmail.com>
2026-02-02 00:11:58 +0530

    jwt bcrypt tokens added

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,6 @@
 import moongoose , {Schema} from 'moongoose'
-import mongooseAggregatePaginate from 'mongoose-aggregate-paginate-v2'
-
+import jwt from "jsonwebtokens"
+import bcrypt from "bcrypt"
 
 const videoSchema = new Schema(
     {
@@ -41,7 +41,45 @@ const videoSchema = new Schema(
     },{ timestamps : true }
 )
 
+userSchema.pre("save" , async function(next) {
+    //if there is no change in password field skip
+    if(!this.isModified("password") ) return next();
+    //else if there is changes in pass encrypt again
+    this.password = bcrypt.hash(this.password, 10)  //10 is number of rounds it wither be 8 or like default
+    next()
+})
 
-videoSchema.plugins(mongooseAggregatePaginate)  //plugin
+//custom method : to chk/match that the stored pass is equal to the pass given by user in like login etc
+
+userSchema.methods.isPassword = async function(password){
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAcessToken = function(){
+    return jwt.sign(
+        {
+            id: this._id,  //we get this id form mongoDB
+            email: this.email,
+            username: this.username,
+            fullName: this.fullname
+         },
+         process.env.ACCESS_TOKEN_SECRET,
+         {
+            expiresIn : process.env.ACCESS_TOKEN_EXPIRY
+         }
+    )
+}
+
+userSchema.methods.generateAcessToken = function(){
+    return jwt.sign(
+        {
+            id: this._id,  //we get this id from mongoDB
+         },
+         process.env.REFRESH_TOKEN_SECRET,
+         {
+            expiresIn : process.env.REFRESH_TOKEN_EXPIRY 
+         }
+    )
+}
 
 export const Video = moongoose.model("Video", videoSchema)
