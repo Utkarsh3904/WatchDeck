859894d1ce2a53b0465f83b96a99a10a8c9be9d0
Utkarsh3904 <millenniumatom@gmail.com>
2026-02-04 12:29:23 +0530

    basic route setup

---
+++ src/routes/user.routes.js
@@ -0,0 +1,11 @@
+//it will repeat
+import {Router} from "express"
+import { registerUser } from "../controllers/user.controller.js"
+
+const router = Router()
+
+console.log(registerUser);
+
+router.route("/register").post( registerUser )
+
+export default router 
