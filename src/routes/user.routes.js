5988b57bae9f524f725bb3861d7455def4cc3264
Utkarsh3904 <millenniumatom@gmail.com>
2026-02-04 23:56:50 +0530

    middleware of multer added for register

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,25 @@
 //it will repeat
 import {Router} from "express"
 import { registerUser } from "../controllers/user.controller.js"
-
+import {upload} from "../middlewares/multer.middleware.js"  
 const router = Router()
 
 console.log(registerUser);
 
-router.route("/register").post( registerUser )
+router.route("/register").post( 
+
+    upload.fields([         //middleware added
+        {
+            name : "avatar",
+            maxCount : 1
+        },
+        {
+            name: "coverImage",
+            maxCount: 1
+
+        }
+    ]),
+    registerUser 
+)
 
 export default router 
