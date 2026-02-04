2989b3b32f8daf073c59e88788d3a7b79381afc9
Utkarsh3904 <millenniumatom@gmail.com>
2026-02-04 23:56:19 +0530

    video model fixed

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,54 +1,46 @@
 import moongoose , {Schema} from 'mongoose'
 import mongooseAggregatePaginate from 'mongoose-aggregate-paginate-v2'
 
-
-const userSchema = new Schema(
+const videoSchema = new Schema(
     {
-        username: {
-            type: String,
-            required : true,
-            unique : true,
-            lowercase :true,
-            trim : true,
-            index: true    //for to enable search on basis on this (optimizes search)
-        },
-        email: {
-            type: String,
-            required : true,
-            unique : true,
-            lowercase :true,
-            trim : true,
-        },
-        username: {
-            type: String,
-            required : true,
-            trim : true,
-            index: true    //for to enable search on basis on this (optimizes search)
-        },
-        avatar:{
-            type: String,     //cloudinary URL
+        videoFile : {
+            type: String,   //cloudinary URL
             required: true,
         },
-        coverImage :{
-            type: String,
+        thumbnail : {
+            type: String,   //cloudinary URL
+            required: true,
         },
-        watchHistory : [   //array
-            {
-                type: Schema.Types.ObjectId,
-                ref : "Video"
-            }
-        ],
-        password :{
-            type : String,
-            required: [true, 'Passwrd is required']   //just a message 
+        title : {
+            type: String,   
+            required: true,
         },
-        refreshToken: {
-            type : String
+        description : {
+            type: String,   
+            required: true,
+        },
+        duration : {
+            type: Number,    //Cloudinary URL (when data is stored it stores the time)
+            default : 0   
+            },
+        views : {
+            type: Number,
+            required : true,
+        },
+        isPublished : {
+            type: Boolean,
+            default : true
+        },
+        owner : {
+            type: Schema.Types.ObjectId,
+            ref: "User"
         }
-    },
-    { timestamps : true }
+
+
+    },{ timestamps : true }
 )
+
 
 videoSchema.plugins(mongooseAggregatePaginate)  //plugin
 
-export const User = moongoose.model( "User" , userSchema)
+export const Video = moongoose.model("Video", videoSchema)
