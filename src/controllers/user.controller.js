ffe6c1ff698e673181297819dc0ac809e28bfcec
Utkarsh3904 <millenniumatom@gmail.com>
2026-02-07 01:13:33 +0530

    add token generation and user password verification improvements

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,6 +6,23 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken"
 import mongoose from "mongoose";
 
+const generateAccessAndRefereshTokens = async(userId) =>{
+    try {
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken = refreshToken
+        await user.save({ validateBeforeSave: false })
+
+        return {accessToken, refreshToken}
+
+
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generating referesh and access token")
+    }
+}
+
 const registerUser = asyncHandler( async (req,res) => {
 
     // Steps to follow to register the user : 
